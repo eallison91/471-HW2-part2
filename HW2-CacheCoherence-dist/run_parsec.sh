6768fb3feca293a753473f64c047f46cb149b840
Mark Wyse <muwyse@gmail.com>
2014-05-20 11:36:13 -0700

    modify run_parsec.sh

--- HW2-CacheCoherence-dist/run_parsec.sh
+++ HW2-CacheCoherence-dist/run_parsec.sh
@@ -6,6 +6,6 @@ export LD_LIBRARY_PATH=/cse/courses/cse471/14sp/lib32
  # echo "Usage: ./run_parsec.sh <protocols> <num_caches>"
 #else
 
-parsecmgmt -a run -i test -d /homes/iws/wysem/14sp/cse471/471-HW2-part2/HW2-CacheCoherence-dist/output -n 8 -s 'pin -t /cse/courses/cse471/14sp/Assignments/HW2-CacheCoherence/Release/MultiCacheSim_PinDriver.so -protos /homes/iws/wysem/14sp/cse471/471-HW2-part2/HW2-CacheCoherence-dist/MESI_SMPCache.so -stopOnProtoBug -numcaches 8 -- '
+parsecmgmt -a run -i simsmall -d /homes/iws/wysem/14sp/cse471/471-HW2-part2/HW2-CacheCoherence-dist/output -n 8 -s 'pin -t /cse/courses/cse471/14sp/Assignments/HW2-CacheCoherence/Release/MultiCacheSim_PinDriver.so -protos /homes/iws/wysem/14sp/cse471/471-HW2-part2/HW2-CacheCoherence-dist/MESI_SMPCache.so -stopOnProtoBug -printOnProtoBug -numcaches 8 -- '
 
 #fi
