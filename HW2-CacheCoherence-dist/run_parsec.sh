f4b42f7643b3552c5000a2aa188f90a4ecf376f7
Mark Wyse <muwyse@gmail.com>
2014-05-22 12:38:56 -0700

    update

--- HW2-CacheCoherence-dist/run_parsec.sh
+++ HW2-CacheCoherence-dist/run_parsec.sh
@@ -2,10 +2,7 @@
 
 export PATH=$PATH:/cse/courses/cse471/14sp/Benchmarks/parsec-2.1/bin:/cse/courses/cse471/14sp/PIN/PIN
 export LD_LIBRARY_PATH=/cse/courses/cse471/14sp/lib32
-#if [[ $# -ne 2 ]]; then
- # echo "Usage: ./run_parsec.sh <protocols> <num_caches>"
-#else
 
-parsecmgmt -a run -i simsmall -d /homes/iws/wysem/14sp/cse471/471-HW2-part2/HW2-CacheCoherence-dist/output -n 8 -s 'pin -t /cse/courses/cse471/14sp/Assignments/HW2-CacheCoherence/Release/MultiCacheSim_PinDriver.so -protos /homes/iws/wysem/14sp/cse471/471-HW2-part2/HW2-CacheCoherence-dist/MESI_SMPCache.so -stopOnProtoBug -printOnProtoBug -numcaches 8 -- '
+parsecmgmt -a run -p facesim -i simsmall -d /homes/iws/wysem/14sp/cse471/471-HW2-part2/HW2-CacheCoherence-dist/output_simsmall_8 -n 8 -s 'pin -t /cse/courses/cse471/14sp/Assignments/HW2-CacheCoherence/Release/MultiCacheSim_PinDriver.so -protos /homes/iws/wysem/14sp/cse471/471-HW2-part2/HW2-CacheCoherence-dist/MESI_SMPCache.so,/homes/iws/wysem/14sp/cse471/471-HW2-part2/HW2-CacheCoherence-dist/MSI_SMPCache.so -numcaches 8 -- '
 
-#fi
+#parsecmgmt -a run -i test -d /homes/iws/wysem/14sp/cse471/471-HW2-part2/HW2-CacheCoherence-dist/output -n 8 -s 'pin -t /cse/courses/cse471/14sp/Assignments/HW2-CacheCoherence/Release/MultiCacheSim_PinDriver.so -protos /homes/iws/wysem/14sp/cse471/471-HW2-part2/HW2-CacheCoherence-dist/MESI_SMPCache.so -stopOnProtoBug -printOnProtoBug -numcaches 8 -- '
