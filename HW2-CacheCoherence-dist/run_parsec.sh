491625a9b581407685a55c4f61ef74c06afb9a6e
Eleanor Allison <efa91@attu4.cs.washington.edu>
2014-05-23 11:05:55 -0700

    .

--- HW2-CacheCoherence-dist/run_parsec.sh
+++ HW2-CacheCoherence-dist/run_parsec.sh
@@ -6,7 +6,7 @@ export LD_LIBRARY_PATH=/cse/courses/cse471/14sp/lib32
 if [[ $# -ne 3 ]]; then
     echo 'usage: ./run_parsec.sh <protocols> <num_caches> <work_directory>'
 else
-    parsecmgmt -a run -i simsmall -d $3 -n 2 -s 'pin -t /cse/courses/cse471/14sp/Assignments/HW2-CacheCoherence/Release/MultiCacheSim_PinDriver.so -protos ./$1 -numcaches $2 -- '
+    parsecmgmt -a run -i simsmall -d $3 -n 2 -s 'pin -t /cse/courses/cse471/14sp/Assignments/HW2-CacheCoherence/Release/MultiCacheSim_PinDriver.so -protos $1 -numcaches $2 -- '
 fi
 
 #parsecmgmt -a run -i test -d /homes/iws/wysem/14sp/cse471/471-HW2-part2/HW2-CacheCoherence-dist/output -n 8 -s 'pin -t /cse/courses/cse471/14sp/Assignments/HW2-CacheCoherence/Release/MultiCacheSim_PinDriver.so -protos /homes/iws/wysem/14sp/cse471/471-HW2-part2/HW2-CacheCoherence-dist/MESI_SMPCache.so -stopOnProtoBug -printOnProtoBug -numcaches 8 -- '
