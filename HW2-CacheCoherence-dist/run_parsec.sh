725c5797934c46df3c01b2d9d635f1bc48ba7d15
Mark Wyse <muwyse@gmail.com>
2014-05-23 12:03:02 -0700

    delete bad data, rebuild .so files, revert run_parsec.sh

--- HW2-CacheCoherence-dist/run_parsec.sh
+++ HW2-CacheCoherence-dist/run_parsec.sh
@@ -3,10 +3,4 @@
 export PATH=$PATH:/cse/courses/cse471/14sp/Benchmarks/parsec-2.1/bin:/cse/courses/cse471/14sp/PIN/PIN
 export LD_LIBRARY_PATH=/cse/courses/cse471/14sp/lib32
 
-if [[ $# -ne 3 ]]; then
-    echo 'usage: ./run_parsec.sh <protocols> <num_caches> <work_directory>'
-else
-    parsecmgmt -a run -i simsmall -d $3 -n 2 -s 'pin -t /cse/courses/cse471/14sp/Assignments/HW2-CacheCoherence/Release/MultiCacheSim_PinDriver.so -protos ./$1 -numcaches $2 -- '
-fi
-
-#parsecmgmt -a run -i test -d /homes/iws/wysem/14sp/cse471/471-HW2-part2/HW2-CacheCoherence-dist/output -n 8 -s 'pin -t /cse/courses/cse471/14sp/Assignments/HW2-CacheCoherence/Release/MultiCacheSim_PinDriver.so -protos /homes/iws/wysem/14sp/cse471/471-HW2-part2/HW2-CacheCoherence-dist/MESI_SMPCache.so -stopOnProtoBug -printOnProtoBug -numcaches 8 -- '
+parsecmgmt -a run -i test -d /homes/iws/wysem/14sp/cse471/471-HW2-part2/HW2-CacheCoherence-dist/output_test_8 -n 8 -s 'pin -t /cse/courses/cse471/14sp/Assignments/HW2-CacheCoherence/Release/MultiCacheSim_PinDriver.so -protos /homes/iws/wysem/14sp/cse471/471-HW2-part2/HW2-CacheCoherence-dist/MESI_SMPCache.so,/homes/iws/wysem/14sp/cse471/471-HW2-part2/HW2-CacheCoherence-dist/MSI_SMPCache.so -numcaches 8 -- '
