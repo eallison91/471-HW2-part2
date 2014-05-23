40bc513077665d85e70858934edfe847dc2b41b0
Eleanor Allison <e.allison91@gmail.com>
2014-05-23 10:44:09 -0700

    moved files from separate branch into master

---
+++ HW2-CacheCoherence-dist/parsec_info.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export PATH=$PATH:/cse/courses/cse471/14sp/Benchmarks/parsec-2.1/bin:/cse/courses/cse471/14sp/PIN/PIN
+export LD_LIBRARY_PATH=/cse/courses/cse471/14sp/lib32
+
+parsecmgmt -a info
